dcbdeb59372945b5125b3d0dc930024336d8f8ad
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2025-11-06 18:31:27 +0700

    Ingredient class update

--- src/main/java/doanJava/Ingredient.java
+++ src/main/java/doanJava/Ingredient.java
@@ -10,4 +10,70 @@ package doanJava;
  */
 public class Ingredient {
     
+    private int ingredientId;
+    private String name;
+    private String unit;
+    private double caloriesPerUnit;
+    private double proteinPerUnit;
+    private double carbsPerUnit;
+    private double fatPerUnit;
+
+    public Ingredient() {
+    }
+
+    public int getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(int ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public double getCaloriesPerUnit() {
+        return caloriesPerUnit;
+    }
+
+    public void setCaloriesPerUnit(double caloriesPerUnit) {
+        this.caloriesPerUnit = caloriesPerUnit;
+    }
+
+    public double getProteinPerUnit() {
+        return proteinPerUnit;
+    }
+
+    public void setProteinPerUnit(double proteinPerUnit) {
+        this.proteinPerUnit = proteinPerUnit;
+    }
+
+    public double getCarbsPerUnit() {
+        return carbsPerUnit;
+    }
+
+    public void setCarbsPerUnit(double carbsPerUnit) {
+        this.carbsPerUnit = carbsPerUnit;
+    }
+
+    public double getFatPerUnit() {
+        return fatPerUnit;
+    }
+
+    public void setFatPerUnit(double fatPerUnit) {
+        this.fatPerUnit = fatPerUnit;
+    }
 }
