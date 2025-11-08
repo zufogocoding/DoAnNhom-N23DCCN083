7cb462acc3db410f4ed5363411520e043850eee3
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2025-11-08 23:44:43 +0700

    create IngredientDAO

---
+++ src/main/java/doanJava/IngredientDAO.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package doanJava;
+import java.sql.*;
+import java.util.*;
+/**
+ *
+ * @author phamt
+ */
+public class IngredientDAO {
+    
+}
