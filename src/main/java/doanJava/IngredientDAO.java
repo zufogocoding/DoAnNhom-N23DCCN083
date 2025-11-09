7fb946b9c8d6fcbc2b3030ad4e65455355fe828f
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2025-11-09 23:37:46 +0700

    update IngredientDAO

--- src/main/java/doanJava/IngredientDAO.java
+++ src/main/java/doanJava/IngredientDAO.java
@@ -10,5 +10,23 @@ import java.util.*;
  * @author phamt
  */
 public class IngredientDAO {
-    
+    public Ingredient addIngredient(Ingredient ingredient){
+        String sql = "INSERT INTO Ingredient(name,unit,caloriesPerUnit,proteinPerUnit,carbsPerUnit,fatPerUnit) VALUES(?,?,?,?,?,?,?)";
+        try (Connection conn = SqliteHelper.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstmt.setString(1,ingredient.getName());
+            pstmt.setString(2, ingredient.getUnit());
+            pstmt.setDouble(3, ingredient.getCaloriesPerUnit());
+            pstmt.setDouble(4, ingredient.getCaloriesPerUnit());
+            pstmt.setDouble(5, ingredient.getCaloriesPerUnit());
+            pstmt.setDouble(6, ingredient.getCaloriesPerUnit());
+            pstmt.executeUpdate();
+            
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if (rs.next())  ingredient.setIngredientId(rs.getInt(1));
+            return ingredient;    
+        } catch(SQLException e) {
+            System.err.println("Error adding ingredient");
+            return null;  
+        } 
+    }
 }
