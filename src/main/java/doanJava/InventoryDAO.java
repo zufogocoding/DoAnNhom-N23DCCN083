f39e5229af091723dc84d98bfb58337e11bc9b3c
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2025-11-10 11:32:30 +0700

    add getInventory and mapResult

--- src/main/java/doanJava/InventoryDAO.java
+++ src/main/java/doanJava/InventoryDAO.java
@@ -4,10 +4,34 @@
  */
 package doanJava;
 
+import java.util.*;
+import java.sql.*;
 /**
  *
  * @author phamt
  */
 public class InventoryDAO {
+   public ArrayList<StudentInventory> getInventory(int studentId){
+       ArrayList<StudentInventory> inventory = new ArrayList<>();
+       String sql = "SELECT * FROM Student_Inventory WHERE student_id = ?";
+       
+       try (Connection conn = SqliteHelper.getConnection();PreparedStatement pstmt = conn.prepareStatement(sql)){
+           pstmt.setInt(1,studentId);
+           ResultSet rs = pstmt.executeQuery();
+           while (rs.next()){
+               inventory.add(mapResultSetToInventory(rs));
+           }
+       } catch(SQLException e) {
+            System.err.println("Error getting inventory "+e.getMessage());
+       }
+       return inventory;
+}
    
+   private StudentInventory mapResultSetToInventory(ResultSet rs) throws SQLException {
+        return new StudentInventory(
+                rs.getInt("student_id"),
+                rs.getInt("ingredient_id"),
+                rs.getDouble("quantity")
+        );
+    }
 }
