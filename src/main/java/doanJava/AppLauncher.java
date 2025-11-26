18026d7590b6727851b30489e58c2be576c2c74d
Tuan Ka <n23dccn134@student.ptithcm.edu.vn>
2025-11-26 16:48:14 +0700

    Test giao diện ... test di con cho

---
+++ src/main/java/doanJava/AppLauncher.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package doanJava;
+
+/**
+ *
+ * @author hatua
+ */
+public class AppLauncher {
+    public static void main(String[] args) {
+        // Gọi đến hàm main của file Main cũ
+        main.main(args);
+}
+}
